e004a07efb23af262e5b8e44b33d5a186ca45a55
Scott Zetlan <scott@zetlan.net>
2022-04-05 13:47:30 -0400

    Remove Order dependency on Product

--- src/main/java/net/zetlan/examples/db/Order.java
+++ src/main/java/net/zetlan/examples/db/Order.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class Order extends BaseEntity {
     private Integer userId;
     private List<OrderItem> orderItems = new ArrayList<>();
-    private Integer totalCost = 0;
 
     public Integer getUserId() {
         return userId;
@@ -22,19 +21,10 @@ public class Order extends BaseEntity {
 
     public void setOrderItems(List<OrderItem> orderItems) {
         this.orderItems = orderItems;
-        totalCost = orderItems.stream()
-                .map(orderItem -> orderItem.getQuantity() * orderItem.getProduct().getPriceCents())
-                .reduce(0, Integer::sum);
     }
 
     public List<OrderItem> addItem(OrderItem orderItem) {
         this.orderItems.add(orderItem);
-        this.totalCost = this.totalCost + (orderItem.getQuantity() * orderItem.getProduct().getPriceCents());
         return this.orderItems;
     }
-
-    public Integer getTotalCost() {
-        return totalCost;
-    }
-
 }
