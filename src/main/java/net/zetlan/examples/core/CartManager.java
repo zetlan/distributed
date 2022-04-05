34973b3ba9e7145e69547210936257341ce0dde5
Scott Zetlan <scott@zetlan.net>
2022-04-05 09:26:50 -0400

    Shell for CartManager

---
+++ src/main/java/net/zetlan/examples/core/CartManager.java
@@ -0,0 +1,23 @@
+package net.zetlan.examples.core;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class CartManager extends BaseManager {
+    //TODO: data structure to keep a list of carts
+
+    @Inject
+    public CartManager() {
+
+    }
+
+    // 1. Get a cart for a user
+
+    // 2. Add an item to a cart for a user
+
+    // 3. Remove an item from a cart
+
+    // 4. Clear the cart
+}
+
