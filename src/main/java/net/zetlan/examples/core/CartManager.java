1de64b04a2f9b01f4ee1e31105531bafc97a82d7
CynthiaClementine <39636090+CynthiaClementine@users.noreply.github.com>
2022-04-05 10:10:00 -0400

    Implemented CartManager

--- src/main/java/net/zetlan/examples/core/CartManager.java
+++ src/main/java/net/zetlan/examples/core/CartManager.java
@@ -3,21 +3,62 @@ package net.zetlan.examples.core;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import net.zetlan.examples.db.Cart;
+import net.zetlan.examples.db.User;
+import net.zetlan.examples.db.OrderItem;
+
+import java.util.HashMap;
+import java.util.Map;
+
 @Singleton
 public class CartManager extends BaseManager {
-    //TODO: data structure to keep a list of carts
+    //store the list of carts
+    private static final Map<Integer, Cart> CART_LIST = new HashMap<>();
 
     @Inject
     public CartManager() {
 
     }
 
+    //creates a cart for a user
+    private Cart createCartFor(Integer userID) {
+        this.CART_LIST.put(userID, new Cart());
+        Cart ct = this.CART_LIST.get(userID);
+        ct.setUserId(userID);
+        return ct;
+    }
+
     // 1. Get a cart for a user
+    public Cart getCartFor(Integer userID) {
+        if (!this.hasCartFor(userID)) {
+            this.createCartFor((userID));
+        }
+        return this.CART_LIST.get(userID);
+    }
+
+    public Boolean hasCartFor(Integer userID) {
+        return this.CART_LIST.containsKey(userID);
+    }
 
     // 2. Add an item to a cart for a user
+    public Cart addItemTo(Integer userID, String sku, Integer quantity) {
+        Cart cart = this.getCartFor(userID);
+        cart.addToCart(sku, quantity);
+        return cart;
+    }
 
     // 3. Remove an item from a cart
+    public Cart removeItemFrom(Integer userID, String sku, Integer quantity) {
+        Cart cart = this.getCartFor(userID);
+        cart.removeFromCart(sku, quantity);
+        return cart;
+    }
 
     // 4. Clear the cart
+    public Cart clearCartFor(Integer userID) {
+        Cart cart = this.getCartFor(userID);
+        cart.clearCart();
+        return cart;
+    }
 }
 
--- src/main/java/net/zetlan/examples/db/Cart.java
+++ src/main/java/net/zetlan/examples/db/Cart.java
@@ -1,5 +1,6 @@
 package net.zetlan.examples.db;
 
+import javax.ws.rs.WebApplicationException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,5 +32,20 @@ public class Cart extends BaseEntity {
         Integer currentQuantity = this.skuQuantities.getOrDefault(sku, 0);
         currentQuantity = currentQuantity + quantity;
         this.skuQuantities.put(sku, currentQuantity);
+    }
+
+    public void removeFromCart(String sku, Integer quantity) throws WebApplicationException {
+        //can't remove an item that doesn't exist
+        if (!this.skuQuantities.containsKey(sku)) {
+            throw new WebApplicationException();
+        }
+
+        //can't remove more items than what's in the cart
+        if (this.skuQuantities.get(sku) < quantity) {
+            throw new WebApplicationException();
+        }
+
+        //now that the issues are out of the way, remove the item(s) from the cart
+        this.skuQuantities.put(sku, this.skuQuantities.get(sku) - quantity);
     }
 }
